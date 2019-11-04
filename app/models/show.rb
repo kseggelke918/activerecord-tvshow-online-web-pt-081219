85fc092ada06f479595a31336e46fcc46bee16e4
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-04 18:06:34 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end 
   
   def self.most_popular_show
-    self.highest_rating     
+    highest_rating     
   end 
   
   def self.lowest_rating
@@ -13,7 +13,7 @@ class Show < ActiveRecord::Base
   end 
   
   def self.least_popular_show
-    self.lowest_rating   
+    lowest_rating   
   end 
   
   def self.ratings_sum
