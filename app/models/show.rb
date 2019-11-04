1222a1d9c09098de9baac3242dda379837170495
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-04 18:11:29 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,11 +5,11 @@ class Show < ActiveRecord::Base
   end 
   
   def self.most_popular_show
-      
+    self.where(highest_rating)  
   end 
   
   def self.lowest_rating
-    Show.minimum(:rating)  
+    self.minimum(:rating)  
   end 
   
   def self.least_popular_show
@@ -17,15 +17,15 @@ class Show < ActiveRecord::Base
   end 
   
   def self.ratings_sum
-    Show.sum(:rating)  
+    self.sum(:rating)  
   end 
   
   def self.popular_shows 
-    Show.where("rating > ?", 5)  
+    self.where("rating > ?", 5)  
   end 
   
   def self.shows_by_alphabetical_order
-    Show.order(:name)  
+    self.order(:name)  
   end 
   
   
