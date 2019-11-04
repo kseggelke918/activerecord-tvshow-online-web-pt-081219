cace5a0bfbfc889a1a245fc909aecb1d8c30c4a2
Katie Seggelke <katie.seggelke@gmail.com>
2019-11-04 18:18:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -15,7 +15,7 @@ class Show < ActiveRecord::Base
   end 
   
   def self.least_popular_show
-    lowest_rating   
+    self.where("rating = ?", self.lowest_rating)   
   end 
   
   def self.ratings_sum
